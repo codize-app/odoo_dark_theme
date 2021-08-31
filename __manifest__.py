8aa7fa70aa102c95267531c53ae954e5e83d2bd1
Ignacio Buioli <ibuioli@gmail.com>
2021-08-31 13:30:22 -0300

    feat: initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Dark Backend Theme",
+    'summary': """Odoo Dark Backend Theme""",
+
+    'description': """Dark Backend Theme for Odoo""",
+
+    'author': "Odoo Dev Free, Codize",
+    'category': 'Theme/Backend',
+    'version': '14.0.0.0.1',
+    'license': 'OPL-1',
+    'price': 0,
+    'currency': 'USD',
+    'images': ['static/description/odf-dark-backend-theme-banner.png'],
+    'depends': ['base', 'mail', 'web'],
+    'data': [
+        'views/dark_theme.xml',
+    ],
+}
